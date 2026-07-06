221619a72c3874988fcb03580fa57d1dfd606bcd
Ishan Sharma <ishansharma7162@gmail.com>
2026-07-06 16:51:14 +0530

    Add files via upload

---
+++ day13.py
@@ -0,0 +1,28 @@
+a = "IShan"
+print(a.upper()) # output: ISHAN
+print(a.lower()) # output: ishan
+b = "    hello   "
+print(b.strip()) 
+c = "Ishan bhai"
+print(c.replace("bhai", "sir"))
+d = "Ishan is goat"
+print(d.split(" ")) 
+e = "Ishan"
+print(e.count("s"))
+f = "Welcome to Python"
+print(f.endswith("Python"))
+# String Methods Practice
+msg = "!!!Hey Ishan!!!"
+
+print("Original:", msg)
+print("Upper Case:", msg.upper())
+print("Right Strip (! hatana):", msg.rstrip("!"))
+print("Replace:", msg.replace("Ishan", "Coder"))
+
+intro = "b.Tech 1st year coding"
+print("Capitalize:", intro.capitalize()) # B bada ho jayega
+
+# Find method test
+print("Ishan ka index:", msg.find("Ishan"))
+
+
