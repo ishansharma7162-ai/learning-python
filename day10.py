3a27d12b4117cedc5ad4713be3d041f7c484b0d0
Ishan Sharma <ishansharma7162@gmail.com>
2026-07-06 13:36:03 +0530

    Add files via upload

---
+++ day10.py
@@ -0,0 +1,17 @@
+variable_name = input("Ishan: ")
+print("hello", variable_name)
+x = input("pehla number: ") # maine dala 100
+y = input("dusra number: ") # maine dala 200
+print(x + y) # output: 100200
+a = input("first number: ") # maine dala 120
+b = input("second number: ") # maine dala 000
+print(a + b) # output: 120000
+p = int( input("third number: ")) # maine dala 1000
+q = int(input("fourth number:")) # maine dala 2000 
+print(p + q) # output: 3000
+user_name = input("enter your name: ")
+print("welcome to B.Tech", user_name)
+# ab ek real calculator banate hain jo user se number lega 
+num5 = int(input("fifth number: ")) # maine daala 79090
+num6 = int(input("sixth number: ")) # maine daala 9090
+print(num5 - num6) # output: 70000
