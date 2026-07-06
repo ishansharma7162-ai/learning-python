6839b85c436e385278daad5cb60e31b2ce70372d
Ishan Sharma <ishansharma7162@gmail.com>
2026-07-06 15:53:45 +0530

    Add files via upload

---
+++ day11.py
@@ -0,0 +1,8 @@
+# multiline string test 
+story = """btech ke 1st year mai hu jitna mujhse ho sakega usse jyada karne ka try karunga luck to secondary thing h jo mujhe chahiye uske layak to bann jau mai"""
+print(story)
+# Indexing test 
+my_fav cricketer = "MSD"
+print(my_fav cricketer)
+
+   
---
+++ day12.py
@@ -0,0 +1,15 @@
+name = "Ishan"
+print(len(name)) # output aayega: 5 (kyuki ishan mai 5 akshar hain)
+fruit = "Mango" 
+print(fruit[0:4]) # ye 0 se 3 print karega mang
+print(fruit[1:4]) # ye 1 se 4 print karega ang
+name = "Ishan"
+print("Length of name is:",len(name)) # output: 5
+# normal slicing 
+print(name[0:3]) # output: Ish (0,1,2,)
+print(name[2:]) # output: a (2 se end tak)
+# Negative slicing challege 
+# name[-4:-2]) yaani (5-4):(5-2) -> 1:3
+print(name[-4:-2]) # output: sh (Index 1 aur 2)
+
+      
